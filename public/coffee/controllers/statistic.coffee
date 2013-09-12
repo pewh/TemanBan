c468bff650cf58d24b2bc11b192846ea4358de9d
Pewh Gosh <25minutes.again@gmail.com>
2013-09-12 21:58:49 +0700

    Sales ratio now fixed

--- public/coffee/controllers/statistic.coffee
+++ public/coffee/controllers/statistic.coffee
@@ -1,13 +1,35 @@
 app.controller 'StatisticController', ($scope, Restangular) ->
-    $scope.asset = [
-        name: 'Beli'
-        y: 23
-        color: '#d9534f'
-    ,
-        name: 'Jual'
-        y: 53
-        color: '#1f6377'
-    ]
-
     Restangular.one('purchase/range').get().then (data) ->
         $scope.transaction = data
+
+        $scope.purchaseTotal = _.reduce data[0].data, (prev, key) ->
+            prev + key.y
+        , 0
+
+        $scope.salesTotal = _.reduce data[1].data, (prev, key) ->
+            prev + key.y
+        , 0
+
+        $scope.calcTotal = $scope.salesTotal - $scope.purchaseTotal
+
+        if $scope.calcTotal < 0
+            $scope.currencyStatus = 'Kerugian'
+            $scope.calcTotal = Math.abs($scope.calcTotal)
+        else
+            $scope.currencyStatus = 'Keuntungan'
+
+        total = $scope.purchaseTotal + $scope.salesTotal
+
+        salesRatio =
+            purchase: $scope.purchaseTotal / total * 100
+            sales: $scope.salesTotal / total * 100
+
+        $scope.asset = [
+            name: 'Beli'
+            y: salesRatio.purchase
+            color: '#d9534f'
+        ,
+            name: 'Jual'
+            y: salesRatio.sales
+            color: '#1f6377'
+        ]
--- public/js/app.js
+++ public/js/app.js
@@ -1223,19 +1223,38 @@
   });
 
   app.controller('StatisticController', function($scope, Restangular) {
-    $scope.asset = [
-      {
-        name: 'Beli',
-        y: 23,
-        color: '#d9534f'
-      }, {
-        name: 'Jual',
-        y: 53,
-        color: '#1f6377'
-      }
-    ];
     return Restangular.one('purchase/range').get().then(function(data) {
-      return $scope.transaction = data;
+      var salesRatio, total;
+      $scope.transaction = data;
+      $scope.purchaseTotal = _.reduce(data[0].data, function(prev, key) {
+        return prev + key.y;
+      }, 0);
+      $scope.salesTotal = _.reduce(data[1].data, function(prev, key) {
+        return prev + key.y;
+      }, 0);
+      $scope.calcTotal = $scope.salesTotal - $scope.purchaseTotal;
+      if ($scope.calcTotal < 0) {
+        $scope.currencyStatus = 'Kerugian';
+        $scope.calcTotal = Math.abs($scope.calcTotal);
+      } else {
+        $scope.currencyStatus = 'Keuntungan';
+      }
+      total = $scope.purchaseTotal + $scope.salesTotal;
+      salesRatio = {
+        purchase: $scope.purchaseTotal / total * 100,
+        sales: $scope.salesTotal / total * 100
+      };
+      return $scope.asset = [
+        {
+          name: 'Beli',
+          y: salesRatio.purchase,
+          color: '#d9534f'
+        }, {
+          name: 'Jual',
+          y: salesRatio.sales,
+          color: '#1f6377'
+        }
+      ];
     });
   });
 
